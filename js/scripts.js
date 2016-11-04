b2716a20b2d50f86e6791141ae0d13965cf26ac1
bokochaos <bokochaos@gmail.com>
2016-11-04 02:23:07 -0700

    Alternative CSS-only slideout

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 /* Adapted from: http://materializecss.com/side-nav.html */
 
 // Initialize collapse button
-$('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-    }
-  );
-  
-$("#greeting").click(function(){
-		$("#greeting").html("世界、こんいちわ");
-	}
-);
+$(document).ready(function(){
+	$('.button-collapse').sideNav({
+		  menuWidth: 300, // Default is 240
+		  edge: 'right', // Choose the horizontal origin
+		  closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+		  draggable: true // Choose whether you can drag to open on touch screens
+		}
+	  );
+	  
+	$("#greeting").click(function(){
+			$("#greeting").text("世界、こんいちわ！");
+		}
+	); 
+});
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //$('.collapsible').collapsible();
 
